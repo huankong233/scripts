96610921009c1a25263d85820fbe6911c5f65fb9
huan_kong <2564076459@qq.com>
2023-02-03 17:03:45 +0800

    m3u8

---
+++ download_m3u8/m3u8.py
@@ -0,0 +1,48 @@
+import requests
+import os
+from pathlib import Path
+import datetime
+from tqdm import tqdm
+import urllib3
+urllib3.disable_warnings()  # 禁用证书认证和警告
+
+
+def download(m3u8_list, save_path):
+    path = Path(save_path)
+    # 如果文件夹不存在，则创建
+    if not path.is_dir():
+        os.mkdir(save_path)
+    for i in range(len(m3u8_list)):
+        print(f'开始下载{m3u8_list[i]}')
+        start = datetime.datetime.now().replace(microsecond=0)
+        ts_urls = []
+        m3u8 = requests.get(url=m3u8_list[i])
+        content = m3u8.text.split('\n')
+        for s in content:
+            # 如果是注释就跳过
+            if s and s.find('#') == -1:
+                ts_urls.append(s)
+        download_ts(ts_urls, down_path=f"{save_path}video{str(i+1)}.ts")
+        end = datetime.datetime.now().replace(microsecond=0)
+        print(f'耗时：{end - start}')
+
+
+# 下载文件并拼接
+def download_ts(ts_urls, down_path):
+    file = open(down_path, 'wb')
+    for i in tqdm(range(len(ts_urls))):
+        ts_url = ts_urls[i]
+        # 可以开启，但是速度会变慢很多
+        # time.sleep(1)
+        try:
+            response = requests.get(url=ts_url, stream=True, verify=False)
+            file.write(response.content)
+        except Exception as e:
+            print('异常请求：%s' % e.args)
+    file.close()
+
+
+if __name__ == '__main__':
+    save_path = 'D:/'
+    download(
+        ["https://978212-1316292924.cos.ap-beijing.myqcloud.com/2S3L133610.m3u8"], save_path)
