6dddd67adb07370a292ba7e0abbf6eb90bda6158
huan_kong <2564076459@qq.com>
2023-07-08 21:46:48 +0800

    更新

--- extract/main.sh
+++ extract/main.sh
@@ -1,6 +1,31 @@
-ext=".tar"
-password=""
-dir=""
+#!/bin/bash
 
-cd $dir
-for gz in *$ext; do extract $gz -P $password; done
+# 这个脚本是用于批量解压文件的
+# 使用方法：bash decompress.sh [后缀] [目录] [密码]
+# 例如：bash decompress.sh zip /home/user 123456
+
+# 检查是否提供了后缀和目录
+if [ $# -lt 2 ]; then
+  echo "缺少参数"
+  echo "使用方法：bash decompress.sh [后缀] [目录] [密码]"
+  exit 1
+fi
+
+# 将参数赋值给变量
+extension=$1
+dir=$2
+password=$3
+
+for file in $dir/*.$extension; do
+  # 检查文件是否存在
+  if [ -f "$file" ]; then
+    if [ -z "$password" ]; then
+      7z x "$file"
+    else
+      7z x "$file" -p$password
+    fi
+    echo "解压了 $file"
+  fi
+done
+
+echo "完成！"
