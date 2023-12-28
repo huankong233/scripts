30b4d32686826d5866a95bf933e7846393b6016e
huan_kong <2564076459@qq.com>
2023-12-28 10:57:32 +0800

    优化使用体验

--- baicizhanExport/main.py
+++ baicizhanExport/main.py
@@ -1,38 +1,95 @@
 import sqlite3
 import json
+import re
 
-baicizhantopicproblem = "./baicizhantopicproblem.db"
-lookup = "./lookup.db"
+baicizhantopicproblem = (
+    input("请输入baicizhantopicproblem.db路径:") or "./baicizhantopicproblem.db"
+)
+baicizhantopicproblemConn = sqlite3.connect(baicizhantopicproblem)
+baicizhantopicproblemCursor = baicizhantopicproblemConn.cursor()
+
+baicizhandoexampleinfo = (
+    input("请输入baicizhandoexampleinfo.db路径:") or "./baicizhandoexampleinfo.db"
+)
+baicizhandoexampleinfoConn = sqlite3.connect(baicizhandoexampleinfo)
+baicizhandoexampleinfoCursor = baicizhandoexampleinfoConn.cursor()
+
+lookup = input("请输入lookup.db路径:") or "./lookup.db"
+lookupConn = sqlite3.connect(lookup)
+lookupCursor = lookupConn.cursor()
+
+# 获得已背词库
+baicizhantopicproblemCursor.execute(
+    "SELECT name FROM sqlite_master WHERE type='table' order by name"
+)
+databases = baicizhantopicproblemCursor.fetchall()
+databases = filter(
+    lambda value: value[0].startswith("ts_learn_offline_dotopic_sync_ids_"),
+    databases,
+)
 
 
-conn1 = sqlite3.connect(baicizhantopicproblem)
-#产生游标
-cur = conn1.cursor()
-#获得已背单词id
-cur.execute("SELECT topic_id FROM ts_learn_offline_dotopic_sync_ids_575 WHERE do_num > 0 ORDER BY create_at")
-wordId = cur.fetchall()
-#关闭链接
-conn1.close()
-#从lookup表中寻找已经背的单词表
-conn2 = sqlite3.connect(lookup)
-cur = conn2.cursor()
-sum = 0
+def getBaicizhandoexampleinfo(id):
+    # 获得已背词库
+    baicizhandoexampleinfoCursor.execute(
+        f"SELECT * FROM ZBOOKFINISHINFO WHERE book_id = {id}"
+    )
+    return baicizhandoexampleinfoCursor.fetchall()[0][2]
+
+
+def get_databases(database):
+    database = {"table_name": database[0]}
+    database["id"] = int(
+        re.match(
+            r"ts_learn_offline_dotopic_sync_ids_(.*)", database["table_name"]
+        ).group(1)
+    )
+    database["name"] = getBaicizhandoexampleinfo(database["id"])
+    return database
+
+
+databases = map(get_databases, databases)
+databases = list(databases)
+
+print("\n现有以下数据库:")
+print("数据库ID\t书名")
+
+for database in databases:
+    print(f"{database['id']}\t\t{database['name']}")
+
+print("\n")
+
+databaseID = input("请输入需要导出的数据库ID:")
+if databaseID == "" or databaseID is None:
+    raise Exception("未输入数据库ID")
+
+database = next(
+    (database for database in databases if database["id"] == int(databaseID)), None
+)
+
+if database is None:
+    raise Exception("输入的数据库ID有误")
+
+baicizhantopicproblemCursor.execute(
+    f"SELECT topic_id FROM {database['table_name']} WHERE do_num > 0 ORDER BY create_at"
+)
+words = baicizhantopicproblemCursor.fetchall()
+
+
+def get_word(topic_id):
+    lookupCursor.execute(
+        f"SELECT word,accent,mean_cn FROM dict_bcz WHERE topic_id = {topic_id}"
+    )
+    return lookupCursor.fetchall()[0]
+
+
 data = []
 
-for word in wordId:
-    cur.execute(f"SELECT word,accent,mean_cn FROM dict_bcz WHERE topic_id = {word[0]}")
-    word = cur.fetchall()
-    Q = word[0][0]
-    A = word[0][2]
-    data.append({
-      "Q":Q,
-      "A":A
-    })
-    sum += 1
+for word in words:
+    word = get_word(word[0])
+    data.append({"Q": word[0], "A": word[2]})
 
-conn2.close()
+file = open("data.json", "w", encoding="utf-8")
+file.write(json.dumps(data, ensure_ascii=False, indent=2))
 
-file = open('data.json','w',encoding="utf-8")
-file.write(json.dumps(data, ensure_ascii=False,indent=2))
-
-print(f"共{sum}个单词")
+print(f"共{len(data)}个单词")
